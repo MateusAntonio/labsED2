cb1dc27b9a29325b99f13b800ccc52c645976915
Mateus Antônio <mateus_antonio3@hotmail.com>
2018-10-17 18:58:47 -0300

    Aula07Lab

---
+++ Aula07Lab/ED2_Lab07_Template/PQ.c
@@ -0,0 +1,86 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "event.h"
+#include "PQ.h"
+
+// TODO: Aqui você deve implementar uma fila com prioridade mínima para
+//       ordenar os eventos por tempo, isto é, o evento com o menor tempo tem
+//       a maior prioridade. Veja as funções definidas em 'event.h' para
+//       a manipulação das estruturas de evento. A princípio, as funções já
+//       existentes em 'event.h' são suficientes para implementar a simulação,
+//       isto é, você não precisa implementar mais nada lá.
+//
+//       Você é livre para implementar a fila com prioridade da forma que quiser
+//       mas é recomendado usar um min-heap para obter um bom desempenho. As
+//       simulações maiores geram uma quantidade grande de eventos: um
+//       limite superior frouxo (mas seguro) para o número máximo simultâneo de
+//       eventos é N^3, aonde N é o número de partículas.
+
+// TODO: Crie a struct pq.
+struct pq {
+    Event* events;
+    int size;
+}
+/*
+ * Cria uma nova fila de prioridade mínima com o limite de elementos informado.
+ */
+PQ* PQ_create(int max_N) {
+    // TODO: Implemente a criação da fila que suporta no máximo o número de
+    //       de eventos informados no parâmetro.
+    PQ* pq = malloc(sizeof(pq));
+    pq->events = malloc(max_N+1 * sizeof(Event));
+    pq->size = 0;
+    return pq;
+}
+
+/*
+ * Libera a memória da fila.
+ */
+void PQ_destroy(PQ *pq) {
+    // TODO: Implemente essa função que libera toda a memória da fila,
+    //       destruindo inclusive os eventos que estavam na fila.
+    if(PQ_is_empty(pq))
+        return;
+    
+    for(int i = 0; i < pq->size; i++)
+        destroy_event(pq->events[i]);
+    free(pq);
+}
+
+/*
+ * Insere o evento na fila segundo o seu tempo.
+ */
+void PQ_insert(PQ *pq, Event *e) {
+    // TODO: Implemente essa função que insere o evento dado na fila segundo
+    //       o tempo do evento.
+    //       Assuma que 'e' não é nulo. É importante testar overflow (inserção
+    //       em uma fila que já contém o número máximo de eventos) para evitar
+    //       dores de cabeça com acessos inválidos na memória.
+    if(PQ_size(pq) < pq->max){
+
+    }
+}
+
+/*
+ * Remove e retorna o evento mais próximo.
+ */
+Event* PQ_delmin(PQ *pq) {
+    // TODO: Implemente essa função que remove o evento com o menor tempo da
+    //       fila e o retorna.
+}
+
+/*
+ * Testa se a fila está vazia.
+ */
+bool PQ_is_empty(PQ *pq) {
+    // TODO: Implemente essa função.
+}
+
+/*
+ * Retorna o tamanho da fila.
+ */
+int PQ_size(PQ *pq) {
+    // TODO: Implemente essa função.
+    return pq.size;
+}
